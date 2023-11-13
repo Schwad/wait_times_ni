9540abb85ec5e5efea2daec016270dc918d95a2d
Nick Schwaderer <nick.schwaderer@shopify.com>
2023-11-13 11:42:21 +0000

    let us group by day, and also fluctuate better

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,7 +7,7 @@ class StaticPagesController < ApplicationController
     @hospitals_average_wait_time = Hospital.joins(:wait_times)
                                        .group('hospitals.name')
                                        .select('hospitals.name, AVG(wait_times.value) AS avg_wait')
-    @days_of_week = ("Sunday".."Saturday").to_a
+    @days_of_week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     @data_by_day = @days_of_week.map do |day|
       {
         name: day,
@@ -19,8 +19,8 @@ class StaticPagesController < ApplicationController
     end
 
     @hospitals_trend_wait_times = Hospital.joins(:wait_times)
-                                          .group('hospitals.name, DATE_TRUNC(\'month\', wait_times.created_at)')
-                                          .select('hospitals.name, DATE_TRUNC(\'month\', wait_times.created_at) AS month, AVG(wait_times.value) as avg_wait')
+                                          .group('hospitals.name, DATE_TRUNC(\'day\', wait_times.created_at)')
+                                          .select('hospitals.name, DATE_TRUNC(\'day\', wait_times.created_at) AS day, AVG(wait_times.value) as avg_wait')
   end
 
 end
